ba38e4b4b322cde883f1ad8cb549fd908320e5d1
KimYeonSang <kek5768@naver.com>
2021-07-02 09:44:14 +0900

    django practice

---
+++ GJ_AI_SECOND/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for GJ_AI_SECOND project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GJ_AI_SECOND.settings')
+
+application = get_asgi_application()
