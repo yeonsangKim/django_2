d393836612f45e54e9f6eecc4ae645c976dc3327
KimYeonSang <kek5768@naver.com>
2021-08-03 10:18:07 +0900

    django practice

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Article(models.Model):            #게시글
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                             related_name='article',null=True)      #User  vs 다 로 연결시켜줌, SET_NULL= 작성자 비공개
+
+    title =  models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/',null=True)  #media파일에 article 폴더에 이미지 저장
+    content = models.TextField(null=True)
+
+
+    created_at = models.DateField(auto_now_add=True)     #db가 작성된 시간이 자동으로 저장
--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -283,3 +283,12 @@ let magicGrid = new MagicGrid({
 });
 
 magicGrid.listen();
+
+var masonrys = document.getElementsByTagName("img")
+
+for (let i=0; masonrys.length; i++) {
+  masonrys[i].addEventListener('load', function () {
+    magicGrid.positionItems();
+  }, false)
+}
+
