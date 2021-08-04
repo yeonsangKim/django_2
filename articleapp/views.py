fd7608b629eb4bc453be7a725256ebb87eb0a0e1
KimYeonSang <kek5768@naver.com>
2021-08-04 15:48:17 +0900

    django practice

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -27,3 +27,19 @@ class ArticleDetailView(DetailView):
     model=Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        from django.urls import reverse
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/delete.html'
