54984b3fb90c59992bd14f6055ff9753d05fc881
yeonsangKim <kek5768@naver.com>
2021-08-18 15:09:35 +0900

    django practice

--- articleapp/models.py
+++ articleapp/models.py
@@ -5,11 +5,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):            #게시글
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
                                related_name='article', null=True)     #User  vs 다 로 연결시켜줌, SET_NULL= 작성자 비공개
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
+                                related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)  #media파일에 article 폴더에 이미지 저장
