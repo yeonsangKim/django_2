e9573ac143f03f4d3cdf87aa6187129bcb51ee6b
yeonsangKim <kek5768@naver.com>
2021-08-20 15:44:28 +0900

    django practice

--- projectapp/views.py
+++ projectapp/views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
 from django.views.generic.list import MultipleObjectMixin
@@ -11,6 +10,8 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -18,10 +19,12 @@ class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
     template_name = 'projectapp/create.html'
+
     def get_success_url(self):
-        from django.urls import reverse
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
-class ProjectDetailView(DetailView,MultipleObjectMixin):
+
+
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
@@ -29,8 +32,20 @@ class ProjectDetailView(DetailView,MultipleObjectMixin):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
+        user = self.request.user
+        project = self.object
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user,
+                                                       project=project)
+        else:
+            subscription = None
+
         article_list = Article.objects.filter(project=self.object)
-        return super().get_context_data(object_list=article_list, **kwargs)
+        return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
+                                        **kwargs)
+
 
 class ProjectListView(ListView):
     model = Project
