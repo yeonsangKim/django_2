d647fc0ea9bedfbb796ded01e96b3c5adf863267
yeonsangKim <kek5768@naver.com>
2021-08-20 16:28:32 +0900

    django practice

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,27 +1,37 @@
-# Create your views here.
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.shortcuts import render
 
+# Create your views here.
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView, ListView
+
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
 @method_decorator(login_required, 'get')
 class SubscriptionView(RedirectView):
-
     def get(self, request, *args, **kwargs):
         user = request.user
         project = Project.objects.get(pk=kwargs['project_pk'])
-
-        subscription = Subscription.objects.filter(user=user, project=project)
-
-        if subscription.exists() :
+        subscription = Subscription.objects.filter(user=user,
+                                                   project=project)
+        if subscription.exists():
             subscription.delete()
         else:
-            Subscription(user=user,project=project).save()
-
-
+            Subscription(user=user, project=project).save()
         return super().get(request, *args, **kwargs)
+
     def get_redirect_url(self, *args, **kwargs):
         from django.urls import reverse
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+
+    def get_queryset(self):
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)
+        return article_list
