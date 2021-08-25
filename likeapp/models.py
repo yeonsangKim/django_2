d7362aa12c44b275267e85010c32bd40234e752a
yeonsangKim <kek5768@naver.com>
2021-08-25 14:47:20 +0900

    django practice

--- likeapp/models.py
+++ likeapp/models.py
@@ -6,9 +6,10 @@ from articleapp.models import Article
 
 
 class LikeRecord(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='like_record',null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='like_record', null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='like_record', null=False)
 
-    article=models.ForeignKey(Article,on_delete=models.CASCADE,related_name='like_record',null=False)
-
-    class Meta :
-        unique_together=['user','article']  #user와 article 의 쌍이 유니크하다.
+    class Meta:
+        unique_together = ['user', 'article']
